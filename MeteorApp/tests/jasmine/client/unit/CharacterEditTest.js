8c7b383e2a29cd85ffb975bec432858d3f15c5b3
Jeff Loewen <jsloewen@gmail.com>
2016-02-05 00:18:25 -0600

    some more client unit tests

--- MeteorApp/tests/jasmine/client/unit/CharacterEditTest.js
+++ MeteorApp/tests/jasmine/client/unit/CharacterEditTest.js
@@ -37,8 +37,32 @@ describe("CharacterEdit Component", function() {
         expect($el.length).toEqual(1);
     });
 
-    it("should have h3 Edit Character", function() {
+    it("should have h3 = Edit Character", function() {
         renderWithProps({});
         expect($el.children('h3').text()).toEqual("Edit Character");
+    });
+
+    // this one doesn't pass and I don't know why...
+    //it("should have a button", function() {
+    //    renderWithProps({});
+    //    expect($el.children(":button").length).toEqual(1);
+    //});
+
+    it("should have empty text input if no id is in props", function() {
+        renderWithProps({});
+        expect($el.children("input:text").value).toEqual(undefined);
+    });
+
+    it("should have character name in text input if id is in props", function() {
+        Meteor.call("insertCharacter", defProps);
+        renderWithProps({
+            routeParams: {
+                _id: defProps._id
+            }
+        });
+
+        expect($el.children("input:text").value).toEqual(defProps.name);
+
+        Meteor.call("removeCharacter", defProps);
     });
 });
