f45a4215e7de28f360f7a90be1c2b45ee0b8718a
dennan.hogue <dennan.hogue@gmail.com>
2016-02-04 18:37:46 -0600

    one last ugly merge thing

--- MeteorApp/client/components/appBody.jsx
+++ MeteorApp/client/components/appBody.jsx
@@ -3,14 +3,16 @@ AppBody = React.createClass({
         return (
             <div className="container">
                 <header>
-                    <h1>KARCTURZ</h1>
-                    <AccountsUIWrapper />
-                    <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/character/list">List</a></li>
-                        <li><a href="/character/new">New</a></li>
-                        <li><a href="/campaign/new">New Campaign</a></li>
-                    </ul>
+                    <div><AccountsUIWrapper /></div>
+                    <h1>Slaughter Everything That Is Not An Elf</h1>
+                    <div className="nav">
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/character/list">List</a></li>
+                            <li><a href="/character/new">New</a></li>
+                            <li><a href="/campaign/new">New Campaign</a></li>
+                        </ul>
+                    </div>
                 </header>
                 {this.props.children ? this.props.children : ''}
             </div>
