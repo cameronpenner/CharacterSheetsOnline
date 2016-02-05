b633cdc02fb9f0e6ba33db04dca4de7a9dd68394
= <=>
2016-02-04 22:23:40 -0600

    halfway there. Examples for Tyler.

--- MeteorApp/client/components/character/CharacterEdit.jsx
+++ MeteorApp/client/components/character/CharacterEdit.jsx
@@ -8,6 +8,7 @@ CharacterEdit = React.createClass({
     setCharacter(character) {
         this.data.character = character;
         this.refs.name.value = character.name;
+        this.refs.item.value = "itemName {parameterName, parameterValue}";
     },
 
     getMeteorData() {
@@ -25,6 +26,22 @@ CharacterEdit = React.createClass({
         return data;
     },
 
+    formInventoryItemsList() {
+    	var inventory = this.data.character && this.data.character.inventory;
+    	//console.log(this.data.character);
+    	if (!inventory) return null;
+
+    	var itemStings = new Array();    
+		for (item in inventory.items) {
+        	var itemString = item.name + "\t";
+        	for (parameter in item.parameters) {
+        		itemString += "{" + parameter.name + ", " + parameter.value + "} "; 
+        	}
+        	itemStings.push(itemString);
+        }
+        return itemStings;
+    },
+
     handleSubmit(event) {
         event.preventDefault();
 
@@ -38,16 +55,47 @@ CharacterEdit = React.createClass({
         Character.upsert(character);
     },
 
+    handleAddItem(event) {
+        event.preventDefault();
+
+        var itemString = this.refs.item.value.trim();
+        var regex = /[a-zA-Z]+ {[a-zA-Z]+, [a-zA-Z]+}/ig;
+        var match = regex.exec(itemString);
+
+        console.log(match[0]);
+        console.log(match[1]);
+        console.log(match[2]);
+
+        // if (this.data.character) {
+        //     character = _.extend(this.data.character, character);
+        // }
+        // else if (this.data.currentUser) {
+        //     character.owner = this.data.currentUser;
+        // }
+        // Character.upsert(character);
+    },
+
     render() {
         return (
             <div>
                 <h3>Edit Character</h3>
-                <form className="new-character" onSubmit={this.handleSubmit}>
+                <form className="new-character-name" onSubmit={this.handleSubmit}>
                     <field>
                         <label>Name</label>
                         <input type="text" ref="name" name="name"/>
                     </field>
                     <button type="submit">Save</button>
+                </form>
+                <h5>Inventory</h5>
+                <ul>
+                    {this.formInventoryItemsList()}
+                </ul>
+                <form className="new-item" onSubmit={this.handleAddItem}>
+                    <field>
+                        <label>New item</label>
+                        <input type="text" ref="item" name="item"/>
+                    </field>
+                    <button type="add">Add</button>
                 </form>
             </div>
         );
