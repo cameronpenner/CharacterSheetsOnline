6c70af5f98cc6e29b983ec972bd1291d62ee4410
dennan.hogue <dennan.hogue@gmail.com>
2016-02-05 13:56:10 -0600

    Oops - forgot to change hardcoded player name

--- MeteorApp/client/components/campaign/CampaignList.jsx
+++ MeteorApp/client/components/campaign/CampaignList.jsx
@@ -32,7 +32,7 @@ CampaignList = React.createClass({
     mixins:[ReactMeteorData],
 
     getMeteorData() {
-        const sub = Meteor.subscribe('campaign-list', Meteor.userId(), "dennantest@test.com");
+        const sub = Meteor.subscribe('campaign-list', Meteor.userId(), Meteor.user().emails[0].address);
         return {
             ready: sub.ready(),
             campaigns: Campaign.findAll()
