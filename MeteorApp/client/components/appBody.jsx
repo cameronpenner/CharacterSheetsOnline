9ada9514fc72315758279356e5c9e1f807a78a27
cameron penner <camdpenner@gmail.com>
2016-02-16 16:55:02 -0600

    minor navbar formatting

--- MeteorApp/client/components/appBody.jsx
+++ MeteorApp/client/components/appBody.jsx
@@ -15,24 +15,26 @@ AppBody = React.createClass({
         return (
             <div className="container">
                 <header>
-                    <nav className="nav">
-                        <div className="navbar-header">
-                            <a className="navbar-brand" href="/">RPG Companion</a>
-                        </div>
-                        {this.data.user ?
-                            <div>
-                                <ul className="nav navbar-nav">
-                                    <li><a href="/character/list">Character List</a></li>
-                                    <li><a href="/character/new">New Character</a></li>
-                                    <li><a href="/campaign/list">Campaign List</a></li>
-                                    <li><a href="/campaign/new">New Campaign</a></li>
-                                </ul>
-                                <u1 className="nav navbar-nav navbar-right">
-                                    <li><a href="#" onClick={this.logout}>Logout</a></li>
-                                </u1>
+                    <nav className="navbar navbar-default">
+                        <div className="container-fluid">
+                            <div className="navbar-header">
+                                <a className="navbar-brand" href="/">RPG Companion</a>
                             </div>
-                            : ''
-                        }
+                            {this.data.user ?
+                                <div>
+                                    <ul className="nav navbar-nav">
+                                        <li><a href="/character/list">Character List</a></li>
+                                        <li><a href="/character/new">New Character</a></li>
+                                        <li><a href="/campaign/list">Campaign List</a></li>
+                                        <li><a href="/campaign/new">New Campaign</a></li>
+                                    </ul>
+                                    <u1 className="nav navbar-nav navbar-right">
+                                        <li><a href="#" onClick={this.logout}>Logout</a></li>
+                                    </u1>
+                                </div>
+                                : ''
+                            }
+                        </div>
                     </nav>
                 </header>
                 {this.data.user ? this.props.children : <SignIn/>}
