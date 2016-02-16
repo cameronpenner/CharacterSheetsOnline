71a0a3aa52c10b41ad6789390d7197b9c9364715
cameron penner <camdpenner@gmail.com>
2016-02-16 16:40:01 -0600

    cleaned up navbar a bit

--- MeteorApp/client/components/appBody.jsx
+++ MeteorApp/client/components/appBody.jsx
@@ -15,24 +15,27 @@ AppBody = React.createClass({
         return (
             <div className="container">
                 <header>
-                    <nav className="navbar navbar-default">
+                    <nav className="nav">
                         <div className="navbar-header">
                             <a className="navbar-brand" href="/">RPG Companion</a>
                         </div>
                         {this.data.user ?
-                            <ul className="nav navbar-nav">
-                                <li><a href="/character/list">Character List</a></li>
-                                <li><a href="/character/new">New Character</a></li>
-                                <li><a href="/campaign/list">Campaign List</a></li>
-                                <li><a href="/campaign/new">New Campaign</a></li>
-                                <li><a href="#" onClick={this.logout}>Logout</a></li>
-                            </ul>
+                            <div>
+                                <ul className="nav navbar-nav">
+                                    <li><a href="/character/list">Character List</a></li>
+                                    <li><a href="/character/new">New Character</a></li>
+                                    <li><a href="/campaign/list">Campaign List</a></li>
+                                    <li><a href="/campaign/new">New Campaign</a></li>
+                                </ul>
+                                <u1 className="nav navbar-nav navbar-right">
+                                    <li><a href="#" onClick={this.logout}>Logout</a></li>
+                                </u1>
+                            </div>
                             : ''
                         }
-                        <div className="nav navbar-nav navbar-right"><AccountsUIWrapper /></div>
                     </nav>
                 </header>
-                {this.props.children ? this.props.children : ''}
+                {this.data.user ? this.props.children : <SignIn/>}
             </div>
         );
     }
