dfb6f8cb81ad495eaed094ca83a193341b6cd3a3
Jeff Loewen <jsloewen@gmail.com>
2016-02-26 20:06:53 -0600

    removed unused method

--- MeteorApp/server/character/methods.js
+++ MeteorApp/server/character/methods.js
@@ -56,11 +56,6 @@ Meteor.methods({
         });
     },
 
-    getCharacter: function(_id) {
-        if (!_id) return null;
-        return Characters.findOne(_id);
-    },
-
     removeCharacterAttribute: function(_id, attributeId) {
         if (!_id || !attributeId || !Meteor.user()) return null;
         Meteor.call("removeAttribute", attributeId);
