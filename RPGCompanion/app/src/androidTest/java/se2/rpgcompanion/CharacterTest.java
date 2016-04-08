a7f712623f183f5770822381fccf6cdd26d90ca6
dennan.hogue <dennan.hogue@gmail.com>
2016-04-08 15:45:06 -0500

    Split up character unit tests

--- RPGCompanion/app/src/androidTest/java/se2/rpgcompanion/CharacterTest.java
+++ RPGCompanion/app/src/androidTest/java/se2/rpgcompanion/CharacterTest.java
@@ -1,5 +1,6 @@
 package se2.rpgcompanion;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import se2.rpgcompanion.Pcharacter;
@@ -26,8 +27,13 @@ public class CharacterTest {
     private static final String CAMPAIGN_TWO = "epic campaign of epic";
     private static final String ID_TWO = "9001";
 
+    @Before
+    public void createDefaultCharacter() {
+        testCharacter = new Pcharacter(NAME_ONE, OWNER_ONE, OWNER_NAME_ONE, CREATION_DATE_ONE, CAMPAIGN_ONE, null, null, ID_ONE);
+    }
+
     @Test
-    public void testSetName(){
+    public void testConstructor(){
         testCharacter = new Pcharacter(NAME_ONE, OWNER_ONE, OWNER_NAME_ONE, CREATION_DATE_ONE, CAMPAIGN_ONE, null, null, ID_ONE);
         testCharTwo = new Pcharacter(NAME_TWO, OWNER_TWO, OWNER_NAME_TWO, CREATION_DATE_TWO, CAMPAIGN_TWO, null, null, ID_TWO);
 
@@ -44,17 +50,18 @@ public class CharacterTest {
         assertEquals(CREATION_DATE_TWO, testCharTwo.getCreationDate());
         assertEquals(CAMPAIGN_TWO, testCharTwo.getCampaign());
         assertEquals(ID_TWO, testCharTwo.getId());
+    }
 
-        testCharacter.setName(NAME_TWO);
-        testCharacter.setOwner(OWNER_TWO);
-        testCharacter.setOwnerName(OWNER_NAME_TWO);
-        testCharacter.setCampaign(CAMPAIGN_TWO);
+    @Test
+    public void testGettersAndSetters() {
+        testCharacter.setName("testName");
+        testCharacter.setOwner("testOwner");
+        testCharacter.setOwnerName("testOwnerName");
+        testCharacter.setCampaign("testCampaignName");
 
-        assertEquals(NAME_TWO, testCharacter.getName());
-        assertEquals(OWNER_TWO, testCharacter.getOwner());
-        assertEquals(OWNER_NAME_TWO, testCharacter.getOwnerName());
-        assertEquals(CAMPAIGN_TWO, testCharacter.getCampaign());
-        assertEquals(ID_ONE, testCharacter.getId());
-
+        assertEquals("testName", testCharacter.getName());
+        assertEquals("testOwner", testCharacter.getOwner());
+        assertEquals("testOwnerName", testCharacter.getOwnerName());
+        assertEquals("testCampaignName", testCharacter.getCampaign());
     }
 }
